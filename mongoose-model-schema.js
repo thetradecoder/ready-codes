9d7c55cd68446fa02a42b0b3c22e3bede217c397
Mamun Abdullah <21096193+thetradecoder@users.noreply.github.com>
2021-03-16 15:06:07 +0600

    Update mongoose-model-schema.js

--- mongoose-model-schema.js
+++ mongoose-model-schema.js
@@ -1,1 +1,18 @@
 //mongodb mongoose model schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username:{type:String},
+    email:{type:String},
+    password:{type:String},
+    firstName:{type:String},
+    lastName:{type:String},    
+    mobile:{type:Number} 
+}, {timestamps:true});
+
+const Users = mongoose.model("User", UserSchema);
+
+module.exports = Users;
+
+// change with your own code
