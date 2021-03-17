68827bbf4ba02fae555e2aa2ed3b5ab6a0bb2b91
thetradecoder <thetradecoder@gmail.com>
2021-03-17 19:52:56 +0600

    update localStorage

--- localstorage.js
+++ localstorage.js
@@ -1,1 +1,14 @@
 // Local Storage
+const x = 'x for x-ray';
+
+// set an item named localx in localStorage with value from const x.
+const myStorage = localStorage.setItem('localx', x );
+
+// get an item from local storage
+const localX = localStorage.getItem('localx');
+
+// console.log(localX)  
+// prints (x for x-ray')
+
+
+
