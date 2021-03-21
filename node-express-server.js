ca4e9907893f2d0ab07f18698886052077b059d3
thetradecoder <thetradecoder@gmail.com>
2021-03-21 19:46:38 +0600

    local update

--- node-express-server.js
+++ node-express-server.js
@@ -1,1 +1,29 @@
 // Node Express Server
+
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/users.route');
+require('dotenv').config();
+
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+const mongoUri = process.env.MONGO_URI;
+mongoose.connect(mongoUri, {useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false})
+.then(()=>console.log('- mongo connected!'))
+.catch(err=>mongoose.connection.close());
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+
+
+app.listen(port, ()=>{
+    console.log(`- Server is listening on port ${port}`);
+})
